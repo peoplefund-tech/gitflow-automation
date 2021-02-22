c57895ed3fef8b0c57f7f0bc610de3547d086cff
Sanggyu Kang <other.yuka@gmail.com>
2021-02-22 16:14:40 +0900

    chore: Remove unused variable

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -13,8 +13,7 @@ IS_NEED_APPROVE="false"
 function create_pr()
 {
  TITLE="hotfix auto merged by $(jq -r ".pull_request.head.user.login" "$GITHUB_EVENT_PATH" | head -1)."
- REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
- SOURCE_BRANCH=$(jq -r ".pull_request.head.ref" "$GITHUB_EVENT_PATH")
+ REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")\
  RESPONSE_CODE=$(curl -o $OUTPUT_PATH -s -w "%{http_code}\n" \
   --data "{\"title\":\"$TITLE\", \"head\": \"$BASE_BRANCH\", \"base\": \"$TARGET_BRANCH\"}" \
   -X POST \
